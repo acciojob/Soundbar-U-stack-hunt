7e1a6d841e1b5744a5ae7c7f3d0bda383eb7992a
Uday Kiran <udaydodledodle@gmail.com>
2025-11-28 21:22:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const buttons = document.querySelectorAll('.btn');
+const stopButton = document.querySelector('.stop');
+const sounds = document.querySelectorAll('audio');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        stopSounds();
+        const soundId = button.getAttribute('data-sound');
+        document.getElementById(soundId).play();
+    });
+});
+
+stopButton.addEventListener('click', () => {
+    stopSounds();
+});
+
+function stopSounds() {
+    sounds.forEach(sound => {
+        sound.pause();
+        sound.currentTime = 0;
+    });
+}
